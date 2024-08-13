5db6a89c3f429b3cac1be3338eccd4ad0c3ea3d2
Supul Kalhara <supulkalhara7@gmail.com>
2024-08-13 12:35:55 +0530

    service apis started

---
+++ SalonSpaBookingSystem/DTO/ServiceCreateRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalonSpaBookingSystem.DTO
+{
+    public class ServiceCreateRequestDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int Duration { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
---
+++ SalonSpaBookingSystem/Interfaces/IRepositories/IServiceRepository.cs
@@ -0,0 +1,18 @@
+﻿using SalonSpaBookingSystem.DTO.InternalDTO;
+using SalonSpaBookingSystem.Models;
+
+namespace SalonSpaBookingSystem.Interfaces.IRepositories
+{
+    public interface IServiceRepository
+    {
+        public Task<GeneralResponseInternalDTO> CreateService(ServiceModel service);
+
+        public Task<GeneralResponseInternalDTO> ServiceExist(string serviceId);
+
+        public Task<GeneralResponseInternalDTO> UpdateService(ServiceModel service);
+
+        public Task<GeneralResponseInternalDTO> DeleteService(ServiceModel service);
+
+        public Task<GeneralResponseInternalDTO> FetchServices();
+    }
+}
